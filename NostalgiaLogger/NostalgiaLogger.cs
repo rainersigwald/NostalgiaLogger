b79d557e916a4c950dd5b7514c69772d9edc7512
Rainer Sigwald <raines@microsoft.com>
2024-05-15 12:10:42 -0500

    Initial version
    Sound source: HDD DRIVE working in a hub by Hubertus_Winczyk -- https://freesound.org/s/535981/ -- License: Attribution 4.0

---
+++ NostalgiaLogger/NostalgiaLogger.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+using NAudio.Wave;
+
+using System.IO;
+using System.Reflection;
+using System.Resources;
+using System.Threading;
+
+namespace NostalgiaLogger;
+
+public class NostalgiaLogger : Logger
+{
+    private bool playbackActive;
+
+    private Mp3FileReader audioReader;
+    private WaveOutEvent outputDevice;
+
+    private ResourceManager ResourceManager { get; } = new("NostalgiaLogger.Resources", typeof(NostalgiaLogger).Assembly);
+
+    public override void Initialize(IEventSource eventSource)
+    {
+        eventSource.BuildStarted += OnBuildStarted;
+        eventSource.BuildFinished += OnBuildFinished;
+    }
+
+    private void OnBuildStarted(object sender, BuildStartedEventArgs e)
+    {
+        Thread playbackThread = new(StartPlayback);
+        playbackThread.Start();
+    }
+
+    private void OnBuildFinished(object sender, BuildFinishedEventArgs e)
+    {
+        StopPlayback();
+    }
+
+    private void StartPlayback()
+    {
+        audioReader = new Mp3FileReader(ResourceManager.GetStream("HardDriveSound"));
+        outputDevice = new WaveOutEvent();
+
+        playbackActive = true;
+
+        outputDevice.Init(audioReader);
+
+        while (playbackActive)
+        {
+            outputDevice.Play();
+            while (outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+    public void StopPlayback()
+    {
+        playbackActive = false;
+
+        outputDevice?.Stop();
+
+        outputDevice?.Dispose();
+        audioReader?.Dispose();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NostalgiaLogger
+
+In the old days, you could *tell* when your build was going, because your hard disk drive made sounds when it was working hard. This feedback is lost today, but we can bring it back with NostalgiaLogger.
+
+Build this repo and pass the output to MSBuild as a logger, for example with
+
+```shell
+msbuild -l:S:\NostalgiaLogger\NostalgiaLogger\bin\Debug\net472\NostalgiaLogger.dll
+```
+
+Audio source: <a href="https://freesound.org/people/Hubertus_Winczyk/sounds/535981/">HDD DRIVE working in a hub</a> by <a href="https://freesound.org/people/Hubertus_Winczyk/">Hubertus_Winczyk</a> | License: <a href="https://creativecommons.org/licenses/by/4.0/">Attribution 4.0</a>
